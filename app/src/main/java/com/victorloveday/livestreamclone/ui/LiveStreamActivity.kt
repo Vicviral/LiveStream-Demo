0b2f3a81b7b23620ca42d03fb10a7b76abd9748f
Vicviral <lovedayv7@gmail.com>
2023-01-27 10:30:44 +0100

    subscirbe to channel and update LiveStream activity with recieved messages

--- app/src/main/java/com/victorloveday/livestreamclone/ui/LiveStreamActivity.kt
+++ app/src/main/java/com/victorloveday/livestreamclone/ui/LiveStreamActivity.kt
@@ -3,18 +3,19 @@ package com.victorloveday.livestreamclone.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
 import com.victorloveday.livestreamclone.adapters.MessageListAdapter
 import com.victorloveday.livestreamclone.hideKeyboard
+import com.victorloveday.livestreamclone.showToast
 import io.getstream.chat.android.client.models.Message
 import io.victorloveday.livestreamclone.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val adapter: MessageListAdapter()
+    private val adapter = MessageListAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,13 @@ class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
         messagesListRV.adapter = adapter
 
         val viewModel: LiveStreamViewModel by viewModels()
+        viewModel.viewState.observe(this, Observer { state ->
+            when (state) {
+                is State.Messages -> updateMessageList(state.messages)
+                is State.NewMessage -> updateMessageList(adapter.currentList + state.message)
+                is State.Error -> showToast(state.message)
+            }
+        })
 
         sendMessageButton.setOnClickListener {
             viewModel.sendButtonClicked(messageInput.text.toString())
@@ -49,4 +57,5 @@ class LiveStreamActivity : AppCompatActivity(R.layout.activity_main) {
         val playerOptions = IFramePlayerOptions.Builder().controls(0).rel(0).build()
         mockLiveStreamView.initialize(playerListener, false, playerOptions)
     }
+
 }
